154721a79a1e28161c2401296796a138c0869b07
s15009 <s15009@std.it-college.ac.jp>
2016-08-03 10:22:57 +0900

    Create Account Rayout update

--- app/src/main/java/s14003/std/it_college/ac/jp/pbl2016/LoginActivity.java
+++ app/src/main/java/s14003/std/it_college/ac/jp/pbl2016/LoginActivity.java
@@ -3,6 +3,7 @@ package s14003.std.it_college.ac.jp.pbl2016;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -67,6 +68,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //CreateAccount test
+        //Intent inte = new Intent(this, CreateAccount.class);
+        //startActivity(inte);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
